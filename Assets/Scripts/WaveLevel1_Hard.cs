de860f5b36d3c26e0b1752c8a667bf1caddb296a
OwinBlazer <33065589+OwinBlazer@users.noreply.github.com>
2018-06-29 01:19:41 +0700

    Easy-Hard Levels
    Added easy and hard levels

---
+++ Assets/Scripts/WaveLevel1_Hard.cs
@@ -0,0 +1,245 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveLevel1_Hard : MonoBehaviour {
+
+	BulletSpawn BSPAWN;
+	public GameObject FirstBoss;
+	GetReady GR;
+    //0: Normal - Undeflectable
+    //1: Normal - Deflectable
+    //2: Offlane- Deflectable - Left
+    //3: Offlane- Deflectable - Right
+    //4: Offlane- Undeflectable
+    //5: Split (undeflectable only)
+    //6: Missile (deflectable MUST)
+    //7: Empty / Silent (ada bagian di pattern yang kosong)
+    //8: Ghost - Undeflectable
+    void Start() {
+		//BSPAWN = GameObject.Find ("BulletSpawn").GetComponent<BulletSpawn> ();
+		BSPAWN = FindObjectOfType<BulletSpawn>();
+		GR = FindObjectOfType<GetReady> ();
+
+		//BSPAWN.addBullet (1, 2);
+
+	}
+
+	void Update() {
+		if (!FirstBoss.GetComponent<Enemy> ().isDead() && GR.BeginTheGame == true) {
+			PatternBos1 ();
+		}
+	}
+
+	void PatternBos1() {
+        //1-40
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        //Sampe 150
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(7, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 2);
+
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 4);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 2);
+        BSPAWN.addBullet(1, 2);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(0, 4);
+        BSPAWN.addBullet(1, 0);
+        BSPAWN.addBullet(0, 0);
+        BSPAWN.addBullet(0, 2);
+    }
+
+
+}
