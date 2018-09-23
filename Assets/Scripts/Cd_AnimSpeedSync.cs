f6b0949dec0faa4049542d74a6f4c54e73fcde41
OwinBlazer <33065589+OwinBlazer@users.noreply.github.com>
2018-09-24 00:16:22 +0700

    Fixed Anim Sync, Added Enemy Models
    Anim Sync value fixed for actually working, added 4 new models, each made of sprites

---
+++ Assets/Scripts/Cd_AnimSpeedSync.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cd_AnimSpeedSync : MonoBehaviour {
+    private Animator anim;
+    public float bpm;
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        anim.speed = bpm / 120;
+	}
+}
