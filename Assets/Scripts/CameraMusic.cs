d75ce61b8e8274f7817fb7f2f88959c9d661fbb8
OwinBlazer <33065589+OwinBlazer@users.noreply.github.com>
2018-09-26 20:53:12 +0700

    Fixed Startup Music Loss issue
    Changed some priorities so music starts on first scene load

--- Assets/Scripts/CameraMusic.cs
+++ Assets/Scripts/CameraMusic.cs
@@ -7,16 +7,13 @@ public class CameraMusic : MonoBehaviour {
     public BulletSpawn bulletSpawn;
 	public AudioClip BGM;
 	GetReady GR;
-
-
-	void Awake() {
-		GR = FindObjectOfType<GetReady> ();
-		BackMusic.instance.source.mute = false;
-
-	}
-	void Start () {
-		//StartCoroutine (PlayingMusic ());
-		if (SceneManager.GetActiveScene ().name == "Mainmenu(Demo)" ||
+    
+	void Start ()
+    {
+        GR = FindObjectOfType<GetReady>();
+        BackMusic.instance.source.mute = false;
+        //StartCoroutine (PlayingMusic ());
+        if (SceneManager.GetActiveScene ().name == "Mainmenu(Demo)" ||
 			SceneManager.GetActiveScene ().name == "Options" ||
 			SceneManager.GetActiveScene ().name == "Gameover(Demo)" ||
 			SceneManager.GetActiveScene ().name == "How2Play"||
